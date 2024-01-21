918b2d3ce3a69aff1be460d6ad01fee49e0a05ae
Neveyko Artem <neveiko29@gmail.com>
2024-01-21 14:43:00 +0700

    Improve input

--- RogueLikeRPG/Assets/Scripts/Player.cs
+++ RogueLikeRPG/Assets/Scripts/Player.cs
@@ -14,23 +14,7 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = 1f;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1f;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1f;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = 1f;
-        }
+        Vector2 inputVector = GameInput.Instance.GetMovementVector();
         rb.MovePosition(rb.position + inputVector.normalized * (movingSpeed * Time.fixedDeltaTime));
     }
 }
